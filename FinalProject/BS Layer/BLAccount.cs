edf2f3207bd2e92ded8132d0126f2ab51504123e
letandat2303 <ddat705@gmail.com>
2020-06-23 12:13:24 +0700

    dst1223

---
+++ FinalProject/BS Layer/BLAccount.cs
@@ -0,0 +1,23 @@
+﻿using FinalProject.DB_Layer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.BS_Layer
+{
+    class BLAccount
+    {
+        DBMain db = null;
+        public BLAccount()
+        {
+            db = new DBMain();
+        }
+        public DataTable LAYTAIKHOAN()
+        {
+            return db.ExecuteQueryDataSet("select * from TAIKHOAN", CommandType.Text);
+        }
+    }
+}
---
+++ FinalProject/DB Layer/DBMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.DB_Layer
+{
+    class DBMain
+    {
+        string ConnStr = @"Data Source = .\SQLEXPRESS;Initial Catalog=FinalProject;Integrated Security=True";
+        SqlConnection conn = null;
+        SqlCommand comm = null;
+        SqlDataAdapter da = null;
+        public DBMain()
+        {
+            conn = new SqlConnection(ConnStr);
+            comm = conn.CreateCommand();
+        }
+        public DataTable ExecuteQueryDataSet(string strSQL, CommandType ct)
+        {
+            if (conn.State == ConnectionState.Open)
+                conn.Close();
+            conn.Open();
+            comm.CommandText = strSQL;
+            comm.CommandType = ct;
+            da = new SqlDataAdapter(comm);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        public bool MyExecuteNonQuery(string strSQL, CommandType ct, ref string error)
+        {
+            bool f = false;
+            if (conn.State == ConnectionState.Open)
+                conn.Close();
+            conn.Open();
+            comm.CommandText = strSQL;
+            comm.CommandType = ct;
+            try
+            {
+                comm.ExecuteNonQuery();
+                f = true;
+            }
+            catch (SqlException ex)
+            {
+                error = ex.Message;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return f;
+        }
+    }
+}
